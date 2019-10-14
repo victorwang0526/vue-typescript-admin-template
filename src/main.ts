46bd14a2fd30edc7e3d1d1a53ef7732a84a5b36c
Victor Wang <victor_wang@sottop.cn>
2019-10-14 15:00:26 +0800

    add filters

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import Vue from 'vue'
+import Vue, {DirectiveOptions} from 'vue'
 
 import 'normalize.css'
 import ElementUI from 'element-ui'
@@ -13,6 +13,8 @@ import router from '@/router'
 import i18n from '@/lang'
 import '@/icons/components'
 import '@/permission'
+// import * as directives from '@/directives'
+import * as filters from '@/filters'
 
 Vue.use(ElementUI, {
   i18n: (key: string, value: string) => i18n.t(key, value)
@@ -21,6 +23,16 @@ Vue.use(SvgIcon, {
   tagName: 'svg-icon',
   defaultWidth: '1em',
   defaultHeight: '1em'
+})
+
+// Register global directives
+// Object.keys(directives).forEach(key => {
+//   Vue.directive(key, (directives as { [key: string ]: DirectiveOptions })[key])
+// })
+
+// Register global filter functions
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, (filters as { [key: string ]: Function })[key])
 })
 
 Vue.config.productionTip = false
