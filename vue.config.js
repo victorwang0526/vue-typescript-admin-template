463612d65b53dc1a8aee55b69a95033368502dc3
Victor Wang <victor_wang@sottop.cn>
2019-10-14 12:13:50 +0800

    mock server

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,35 @@
 const path = require('path')
+
+// If your port is set to 80,
+// use administrator privileges to execute the command line.
+// For example, on Mac: sudo npm run / sudo yarn
+const devServerPort = 8080 // TODO: get this variable from setting.ts
+const mockServerPort = 8081 // TODO: get this variable from setting.ts
 const name = 'Vue Typescript Admin'
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/vue-typescript-admin-template/' : '/', // TODO: Remember to change this to fit your need
   lintOnSave: process.env.NODE_ENV === 'development',
+  devServer: {
+    port: devServerPort,
+    open: true,
+    overlay: {
+      warnings: false,
+      errors: true
+    },
+    proxy: {
+      // change xxx-api/login => /mock-api/v1/login
+      // detail: https://cli.vuejs.org/config/#devserver-proxy
+      [process.env.VUE_APP_BASE_API]: {
+        target: `http://localhost:${mockServerPort}/mock-api/v1`,
+        changeOrigin: true, // needed for virtual hosted sites
+        ws: true, // proxy websockets
+        pathRewrite: {
+          ['^' + process.env.VUE_APP_BASE_API]: ''
+        }
+      }
+    }
+  },
   pwa: {
     name: name
   },
